2b39f9880784924a2a390d889f232d704317ce07
Kevin Calmant <kevin.calmant@outlook.com>
2021-03-05 16:44:51 +0100

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,38 @@
+import { Component, Input } from "@angular/core";
+import { timer } from "rxjs";
+import { takeWhile, tap } from "rxjs/operators";
+
+@Component({
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
+})
+export class AppComponent {
+  @Input() maxTime: number = 90;
+
+  animAttribute: string;
+  counter: number = this.maxTime;
+  private _a = 0;
+
+  ngOnInit() {
+    timer(1000, 1000)
+      .pipe(
+        takeWhile(() => this.counter > 0),
+        tap(() => (this.counter -= 1))
+      )
+      .subscribe(() => {
+        this._draw();
+      });
+  }
+
+  private _draw(): void {
+    this._a++;
+    this._a %= 360;
+    const r = (this._a * Math.PI) / (this.maxTime / 2);
+    const x = Math.sin(r) * 125;
+    const y = Math.cos(r) * -125;
+    const mid = this._a > this.maxTime / 2 ? 1 : 0;
+    this.animAttribute =
+      "M 0 0 v -125 A 125 125 1 " + mid + " 1 " + x + " " + y + " z";
+  }
+}
